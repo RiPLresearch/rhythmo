3278bcc0bb61dcd7fc137293595e07015210dbd6
rochelledesilva <rochelleidesilva@gmail.com>
2024-09-18 11:37:57 +1000

    Updated decomp

--- rhythmo/decomp.py
+++ rhythmo/decomp.py
@@ -4,14 +4,14 @@ def create_wavelet(wavelet_waveform):
     """Based on the chosen waveform, creates the relevant wavelet."""
     if wavelet_waveform = "Mortlet":
         WAVELET = cwt.Mortlet(6)
-        elif wavelet_waveform = "Gaussian":
-            WAVELET = cwt.DOG(m=2)
-        elif wavelet_waveform = "Mexican Hat":
-            WAVELET = cwt.MexicanHat()
-        elif wavelet_waveform = "":
-            WAVELET = cwt.
-        else:
-            WAVELET = cwt.Mortlet(6)
+    elif wavelet_waveform = "Gaussian":
+        WAVELET = cwt.DOG(m=2)
+    elif wavelet_waveform = "Mexican Hat":
+        WAVELET = cwt.MexicanHat()
+    elif wavelet_waveform = "":
+        WAVELET = cwt.
+    else:
+        WAVELET = cwt.Mortlet(6)
 
     return WAVELET
 
@@ -64,8 +64,13 @@ def cont_wavelet_transform(resampled_data_array, sampling_interval, WAVELET, fre
         fftfreqs: 
             Fourier frequencies corresponding to fft
     """
-    transformed_wavelet, scales, frequencies_scales, coi, fft, fftfreqs = cwt.cwt(signal = resampled_data_array, dt = sampling_interval, wavelet = WAVELET, freqs = frequencies_cwt) 
-    return transformed_wavelet, scales, coi, fft, fftfreqs
+    #transformed_wavelet, scales, frequencies_scales, coi, fft, fftfreqs = cwt.cwt(signal = resampled_data_array, dt = sampling_interval, wavelet = WAVELET, freqs = frequencies_cwt) 
+    transformed_wavelet, scales, frequencies_scales, _, _, _ = cwt.cwt(signal = resampled_data_array, dt = sampling_interval, wavelet = WAVELET, freqs = frequencies_cwt) 
+
+    return transformed_wavelet, scales, frequencies_scales
+
+
+
 
 def get_global_significance(var, sampling_interval, scales, alpha, DOF, WAVELET):
     """ Global significance of wavelet power spectrum.
@@ -95,9 +100,10 @@ def get_global_significance(var, sampling_interval, scales, alpha, DOF, WAVELET)
                 theoretical wavelet spectrum used for significance testing (often ignored).
     """
     global_significance, wavespec_theor = cwt.significance(var, sampling_interval, scales, 1, alpha, significance_level=0.95, dof=DOF, wavelet=WAVELET)
+    
     return global_significance, wavespec_theor
 
-def get_signifance_levels(sampling_interval, scales, alpha, WAVELET):
+def get_signifance_levels(sampling_interval, scales, alpha, WAVELET): # for fourier transform don't use this
     """ Pointwise significance of wavelet power spectrum.
 
         Parameters
@@ -137,17 +143,25 @@ def decomp(rhythmo_inputs, rhythmo_outputs, parameters):
 
     frequencies_cwt = get_cwt_frequencies(rhythmo_outputs.resampled_data)
 
-    transformed_wavelet, scales, coi, fft, fftfreqs = cont_wavelet_transform(resampled_data_array, sampling_interval, waveform, frequencies_cwt)
+    transformed_wavelet, scales, frequencies_scales = cont_wavelet_transform(resampled_data_array, sampling_interval, waveform, frequencies_cwt)
 
     # Degrees of freedom (DOF) 
-    DOF = resampled_data_array.size - scales  
+    dof = resampled_data_array.size - scales  
     # Variance of resampled data:
     var = resampled_data_array.std()**2 
 
-    global_significance, wavespec_theor = get_global_significance(var, sampling_interval, scales, alpha, DOF, waveform)
+    glbl_power = power.mean(axis=1) # global wavelet power... global power* variance..
+    period = 1 / freqs # goes into dataframe, first column
+    power = glbl_power*var # second column
+    ind_peaks = scipy.signal.find_peaks(var * glbl_power)[0] # detects peaks in the data, indices stored in ind_peaks 
+    peaks = [1 if i in ind_peaks else 0 for i in range(len(period))] # 4th column
 
-    significance_levels, fft_theor = get_signifance_levels(sampling_interval, scales, alpha, waveform)
+    global_significance, wavespec_theor = get_global_significance(var, sampling_interval, scales, alpha, dof, waveform) # 3rd column
 
-    rhythmo_outputs.wavelet_data =  ## some dataframe
+    #significance_levels, fft_theor = get_signifance_levels(sampling_interval, scales, alpha, waveform)
+
+    dataframe = pd.DataFrame({"period": period, "power": power, "significance": global_significance, "peaks": peaks})
+
+    rhythmo_outputs.wavelet_data = dataframe ## some dataframe, 1 column is period, power output from wavelet, significance value at each period, add peak too (output to user)
 
     return rhythmo_outputs
